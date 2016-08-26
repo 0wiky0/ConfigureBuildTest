963479a9b79374bc657e0420a07c5e484d0b2b04
wiky <466543245@qq.com>
2016-08-26 15:25:20 +0800

    commit 1. 区别debug 、release版本

---
+++ ConfigureBuildTest/app/src/main/java/com/wiky/configurebuildtest/MainActivity.java
@@ -0,0 +1,30 @@
+package com.wiky.configurebuildtest;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Util.getTag() 根据其返回的信息，就可以知道当前版本是否切换成功
+                Snackbar.make(view, Util.getTag(), Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
---
+++ ConfigureBuildTest/app/src/release/java/com/wiky/configurebuildtest/Util.java
@@ -0,0 +1,10 @@
+package com.wiky.configurebuildtest;
+
+/**
+ * Created by 4399_wuhui on 2016/8/26.
+ */
+public class Util {
+    public static String getTag(){
+        return "release";
+    }
+}
